10f72166f598d19ef13a00d601453a798f0d5e96
Sidharth Chaudhary <sidharth.chaudhary@ni.com>
2022-11-02 11:59:20 +0530

    first commit

---
+++ src/main/java/com/microservices/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
@@ -0,0 +1,17 @@
+package com.microservices.rest.webservices.restfulwebservices.helloworld;
+
+public class HelloWorldBean {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+}
